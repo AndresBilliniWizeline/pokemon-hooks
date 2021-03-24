da51f3081c0e2aecfb1f0b1f0ef4558e4bb4f7e3
Andrés Billini <ext-a.billini@rappi.com>
2021-03-24 17:57:43 -0500

    Init Class

---
+++ src/components/PokemonClass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PokemonDetailClass extends React.Component {
+  
+
+  render() {
+    let content = <p>Loading Detail...</p>;
+
+    return content;
+  }
+}
+
+export default PokemonDetailClass;
---
+++ src/components/PokemonDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PokemonDetail = ({ selectedPokemon }) => {
+  return (
+    <p>Loading Detail...</p>
+  )
+}
+
+export default PokemonDetail;
